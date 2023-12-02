7e3d16ff99ada79f474596f96c42f96a7c42d6f9
Danil Andreev <61481131+FleshRazer@users.noreply.github.com>
2023-12-02 18:50:27 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+mkdir venv/
+python -m venv venv/
+source venv/bin/activate
+pip install -r requirements.txt
+
 unzip -o -d data/raw data/raw/ml-100k.zip
-mkdir -p data/interim
+mkdir -p data/interim/
